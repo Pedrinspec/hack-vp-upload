176600e294a619ee3022929b6aafb438746d3b11
pedrin <pedrinho.top07@gmail.com>
2026-02-14 00:44:16 -0300

    initial commit

---
+++ src/main/java/com/fiap/vp_upload/VpUploadApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.vp_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/vp_upload/VpUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.vp_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
