be3f416e490a1bb494c7b0d3e7f4cbdf61c71483
pedrin <pedrinho.top07@gmail.com>
2026-03-12 22:10:54 -0300

    Logando a saida da aplicacao

--- src/main/java/com/fiap/vp_upload/infra/adapter/output/KafkaMessageOutputImpl.java
+++ src/main/java/com/fiap/vp_upload/infra/adapter/output/KafkaMessageOutputImpl.java
@@ -5,6 +5,7 @@ import com.fiap.vp_upload.domain.model.ProcessRequest;
 import com.fiap.vp_upload.domain.model.UploadError;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaMessageOutputImpl implements MessageOutput {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
@@ -27,12 +29,14 @@ public class KafkaMessageOutputImpl implements MessageOutput {
     public void sendProcessMessage(ProcessRequest processRequest) {
 
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>(processorTopic, gson.toJson(processRequest));
+        log.info("Enviando mensagem para o tópico {}: {}", processorTopic, producerRecord.value());
         kafkaTemplate.send(producerRecord);
     }
 
     @Override
     public void sendFailMessage(UploadError uploadError) {
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>(notificationTopic, gson.toJson(uploadError));
+        log.info("Enviando mensagem de erro para o tópico {}: {}", notificationTopic, producerRecord.value());
         kafkaTemplate.send(producerRecord);
     }
 }
